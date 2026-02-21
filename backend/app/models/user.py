a27299a2e4610d06c85b51168f582c7fc2a09770
Sagar Singh Gusain <sagargusain201012@gmail.com>
2026-02-22 03:42:15 +0530

    user model for Astra db

---
+++ backend/app/models/user.py
@@ -0,0 +1,21 @@
+
+from uuid import uuid4
+from sqlalchemy import DateTime , Column ,Integer ,String ,Boolean
+from sqlalchemy.sql import func
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String,primary_key = True , index=True , default = lambda:str(uuid4()))
+    email = Column(String,unique = True,nullable = False)
+    hashed_password = Column(String,nullable = False)
+    is_admin = Column(Boolean,default=False)
+    created_at = Column(DateTime(timezone=True) , server_default = func.now())
+
+
+
+
+
+
+
